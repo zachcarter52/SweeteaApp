2d3fe2555a5a624ba3f73da9680d0c5fa94ed279
mariafloresrivera <mariafloresrivera.mfr@gmail.com>
2024-11-17 18:55:52 -0800

    Added Splash Screen and filled out contents of Reward Page

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.core.splashscreen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
@@ -44,6 +45,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
             implementation(projects.shared)
+            implementation(libs.core.splashscreen)
         }
     }
 }
--- composeApp/src/androidMain/kotlin/org/example/sweetea/RewardsPage.kt
+++ composeApp/src/androidMain/kotlin/org/example/sweetea/RewardsPage.kt
@@ -1,17 +1,56 @@
 package org.example.sweetea
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun RewardsPage(){
     Column(
-        Modifier.fillMaxSize()
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Text("Rewards Page")
+        Text("Rewards Program",
+            modifier = Modifier.align(alignment = Alignment.Start),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            )
+        ContentScreen()
+        Text("$1 / 1 Star",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        CustomCircularProgressIndicator(
+            modifier = Modifier
+                .size(250.dp)
+                .background(Color.DarkGray)
+
+            ,
+            initialValue = 0,
+            primaryColor = Color.Blue,
+            secondaryColor = Color.Gray,
+            circleRadius = 230f,
+            onPositionChange = {
+
+            },
+        )
+        Text("Free drink at 75 stars!",
+            modifier = Modifier.padding(16.dp),
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+        )
     }
 }
+
