e20f08e601ddd27cc712b83f8c5717f91e0085f3
zachcarter52 <zachcarter5204@gmail.com>
2025-03-11 13:33:32 -0700

    IOS port structure and location start

---
+++ iosApp/iosApp/Menu.swift
@@ -0,0 +1,19 @@
+//
+//  Menu.swift
+//  iosApp
+//
+//  Created by Zach Carter on 3/7/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
